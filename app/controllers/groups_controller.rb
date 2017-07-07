eaa0c5846014a92071dc38d1308ae731b975f02b
you7588 <yo7588@gmail.com>
2017-07-07 15:03:21 +0800

    implement group#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,14 @@ class GroupsController < ApplicationController
       redirect_to groups_path
   end
 
+  def update
+    @group = Group.find(params[:id])
+
+    @group.update(group_params)
+
+      redirect_to groups_path, notice: "Update Success"
+  end
+
   def edit
     @group = Group.find(params[:id])
   end
