c73f6ca049f3fc9ad3a7d1c8bb0bb40fbaf7aafd
you7588 <yo7588@gmail.com>
2017-07-07 18:14:43 +0800

    add order by creaed_at to group's post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   def index
     @groups = Group.all
   end
@@ -33,7 +34,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts
+    @posts = @group.posts.order("created_at DESC")
   end
 
   def destroy
